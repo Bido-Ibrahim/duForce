b1574fa0cd9428bbe33992500c52a8d40b66ffe4
BMPMS <milesbryony@googlemail.com>
2025-03-25 16:41:21 +0000

    fix: back to original

--- scripts/copy-assets.js
+++ scripts/copy-assets.js
@@ -1,24 +1,6 @@
 const fs = require('fs-extra');
-const path = require('path');
 
-async function copyAssets() {
-  try {
-    // Copy Pixi.js to dist folder
-    // Copy Pixi.js
-    await fs.copy(
-      path.resolve(__dirname, '../node_modules/pixi.js/dist'),
-      path.resolve(__dirname, '../dist/pixi.js')
-    );
-    console.log('Pixi.js assets copied successfully');
-    // Copy Pixi Viewport
-    await fs.copy(
-      path.resolve(__dirname, '../node_modules/pixi-viewport/dist'),
-      path.resolve(__dirname, '../dist/pixi-viewport')
-    );
-    console.log('Pixi-viewport.js assets copied successfully');
-  } catch (err) {
-    console.error('Error copying assets:', err);
-  }
-}
+// Copy assets to 'dist/assets'
+fs.copySync('./assets', './dist/assets');
 
-copyAssets();
+console.log('Assets copied successfully');
