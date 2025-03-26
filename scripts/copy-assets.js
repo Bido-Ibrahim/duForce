b8dc82cbf4c1675cd6cdbbb0221faef1172de7be
BMPMS <milesbryony@googlemail.com>
2025-03-26 18:08:58 +0000

    fix: a few more changes

--- scripts/copy-assets.js
+++ scripts/copy-assets.js
@@ -1,6 +1,6 @@
 const fs = require('fs-extra');
 
 // Copy assets to 'dist/assets'
-fs.copySync('./assets', './dist/assets');
+fs.copySync('./assets', './docs/assets');
 
 console.log('Assets copied successfully');
